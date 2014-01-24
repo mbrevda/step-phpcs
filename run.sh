43c109f6459f9df403d5fe8f362be7b92434e50c
Mike <mrono@mrono.net>
2014-01-24 02:19:01 -0700

    Only check php files

--- run.sh
+++ run.sh
@@ -30,7 +30,7 @@ if [ ! -f vendor/bin/phpcs ]; then
     fail "could not find phpcs, please add it to your composer.json. See here for more: https://github.com/squizlabs/PHP_CodeSniffer#installation"
 fi
 
-vendor/bin/phpcs --standard=$WERCKER_PHPCS_STANDARD --report=$WERCKER_PHPCS_REPORT $WERCKER_PHPCS_IGNORE $WERCKER_PHPCS_DIRECTORY
+vendor/bin/phpcs --extensions=php --standard=$WERCKER_PHPCS_STANDARD --report=$WERCKER_PHPCS_REPORT $WERCKER_PHPCS_IGNORE $WERCKER_PHPCS_DIRECTORY
 
 if [[ $? -ne "0" ]]; then
     fail "PHP CodeSniffer failed";
