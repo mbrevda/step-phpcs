5156920eecaa67198f70d698f0ec1a323bd77b6c
Moshe Brevda <mbrevda@gmail.com>
2014-03-19 08:01:14 +0200

    Add build-esen.sh

--- test.sh
+++ test.sh
@@ -4,30 +4,13 @@
 #test script to mimic fail/warning/info handlers for wercker locally
 
 export WERCKER_PHPCS_DIRECTORY="./src"
-GENERATED_BUILD_NR="1"
 
-info() {
-	echo "info: $1"
-}
-
-warning() {
-	echo "warning: $1"
-}
-
-success() {
-	echo "success: $1"
-	exit 0;
-}
-
-fail() {
-	echo "Failing with: $1"
-	exit 1;
-}
+. build-esen.sh
 
 . run.sh
 
 RESULT=$?
-if [[ $RESULT -ne "0" ]] || [[ $GENERATED_BUILD_NR != "1" ]]; then
+if [[ $RESULT -ne "0" ]]; then
     echo "Test: FAIL -> $RESULT"
     return 1 2>/dev/null || exit 1
 else
